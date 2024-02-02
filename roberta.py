03ee48c4b0211368b7633ec7aa1a4a310630d513
Amit Kumar <amitk6925@gmail.com>
2024-02-02 15:37:20 +0530

    download model if locally not available

--- roberta.py
+++ roberta.py
@@ -1,5 +1,6 @@
 import torch
 import pickle
+import os
 from transformers import XLMRobertaTokenizer, XLMRobertaForSequenceClassification
 
 # Check if CUDA (GPU) is available, otherwise use CPU
@@ -14,16 +15,34 @@ class Roberta:
         self.label_encoder = None
 
     def load(self, model_path):
-        # Load the tokenizer and model
-        self.tokenizer = XLMRobertaTokenizer.from_pretrained("-".join(model_path.split("-")[:3]))
-        self.model = XLMRobertaForSequenceClassification.from_pretrained(model_path)  # Replace with your actual path
+        # Check if model files exist at the provided path
+        if os.path.exists(os.path.join(model_path, "model.safetensors")):
+            print(f"Loading model from local path: {model_path}")
+
+            model_name = "-".join(model_path.split("-")[:3])  # Extract model name from path
+
+            # Load model and tokenizer
+            self.tokenizer = XLMRobertaTokenizer.from_pretrained(model_name)
+            self.model = XLMRobertaForSequenceClassification.from_pretrained(model_path)
+
+        else:
+            model_name = "-".join(model_path.split("-")[:3])  # Extract model name from path
+            print(f"Model files not found at {model_path}. Downloading from Hugging Face...")
+
+            # Download model and tokenizer directly
+            self.tokenizer = XLMRobertaTokenizer.from_pretrained(model_name)
+            self.model = XLMRobertaForSequenceClassification.from_pretrained(model_name)
+
+            # Save the downloaded model to the specified path
+            self.model.save_pretrained(model_path)
+
         self.model.to(device)
 
         # Load the label encoder from the file
         self.label_encoder = pickle.load(open(model_path + "/label_encoder.pkl", "rb"))
         return True
 
-    def inference(self, text):
+    def inference(self, text, k=3):
         # Tokenize the input text
         encoded_text = self.tokenizer(text, padding=True, truncation=True, return_tensors="pt")
         encoded_text.to(device)
@@ -32,11 +51,11 @@ class Roberta:
         with torch.no_grad():
             outputs = self.model(encoded_text.input_ids.to(device),
                                  attention_mask=encoded_text.attention_mask.to(device))
-
         predicted_probabilities = torch.softmax(outputs.logits, dim=1)
 
         # Get the top three predicted classes and their corresponding probabilities
-        top_classes = torch.topk(predicted_probabilities, 3, dim=1)
+        k = k if k <= predicted_probabilities.shape[1] else predicted_probabilities.shape[1]
+        top_classes = torch.topk(predicted_probabilities, k, dim=1)
         top_class_indices = top_classes.indices[0].tolist()
         top_class_probabilities = top_classes.values[0].tolist()
 
